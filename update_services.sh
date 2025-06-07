44fac4881a378966fd630312d99282ae32f0917f
Jehoshaphat Obol <86023492+Jehoshaphat-Obol@users.noreply.github.com>
2025-06-08 01:36:21 +0300

    fix - peformed code linting

--- update_services.sh
+++ update_services.sh
@@ -7,12 +7,15 @@ LOG_FILE="/var/log/mini-netumo-update.log"
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
 {
-  echo "[$DATE] Pulling latest Docker images..."
-} >> "$LOG_FILE"
+    echo "[$DATE] Pulling latest Docker images..."
+} >>"$LOG_FILE"
 
-docker-compose pull >> "$LOG_FILE" 2>&1
+docker-compose pull >>"$LOG_FILE" 2>&1
 
-echo "[$DATE] Restarting services..." >> "$LOG_FILE"
-docker-compose up -d >> "$LOG_FILE" 2>&1
-
-echo "[$DATE] Update complete." >> "$LOG_FILE"
+{
+    echo "[$DATE] Restarting services..."
+} >>"$LOG_FILE"
+docker-compose up -d >>"$LOG_FILE" 2>&1
+{
+    echo "[$DATE] Update complete."
+} >>"$LOG_FILE"
